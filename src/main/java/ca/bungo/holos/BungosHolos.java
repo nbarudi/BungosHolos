969b2621752d7a5d85367a1756414175226754af
Nick <nbarudi@outlook.com>
2025-04-09 19:38:48 -0400

    TG-1 (#done) Created base project

---
+++ src/main/java/ca/bungo/holos/BungosHolos.java
@@ -0,0 +1,12 @@
+package ca.bungo.holos;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BungosHolos extends JavaPlugin {
+
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+}
